99fcdbc44b464ed0982a53057139cec58d3e7844
Nicolas Jiang <nicolas.jiang@9spokes.com>
2018-09-11 14:26:51 +1200

    init

---
+++ packages/ts-b/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var ts_a_1 = __importDefault(require("@nic/ts-a"));
+console.log(ts_a_1.default(1, 2));
---
+++ packages/ts-b/src/index.ts
@@ -0,0 +1,3 @@
+import add from '@nic/ts-a';
+
+console.log(add(1, 2));
